963333b13a3455c14f4ccebcdd11ff1807b40594
Sumit Yadav <ahsiray79@gmail.com>
2024-04-01 16:40:24 +0545

    blog mngt register page

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 export const ForgetPassword = () => {
   return (
@@ -7,10 +9,7 @@ export const ForgetPassword = () => {
         <div className="card shadow">
           <div className="card-body">
             <div className="row d-flex justify-content-center align-items-center">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Blogger_icon_2017.svg/2048px-Blogger_icon_2017.svg.png"
-                style={{ maxWidth: "75px" }}
-              />
+              <img src={Logo} style={{ maxWidth: "75px" }} />
 
               <h2 className="text-center mt-2">Forget Password</h2>
               <form className="mb-3">
@@ -28,9 +27,9 @@ export const ForgetPassword = () => {
               </form>
               <hr />
               <div className="d-flex justify-content-center">
-                <a href="/login" className="text-decoration-none">
+                <Link to="/login" className="text-decoration-none">
                   Login
-                </a>
+                </Link>
               </div>
             </div>
           </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 export const Register = () => {
   return (
-    <div className="  d-flex justify-content-center align-items-center min-vh-100">
+    <div className=" base d-flex justify-content-center align-items-center min-vh-100">
       <div className="col-sm-4">
         <div className="card shadow">
           <div className="card-body">
             <div className="row d-flex justify-content-center align-items-center">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Blogger_icon_2017.svg/2048px-Blogger_icon_2017.svg.png"
-                style={{ maxWidth: " 75px" }}
-              />
+              <img src={Logo} style={{ maxWidth: " 75px" }} />
 
               <h2 className="text-center mt-2">Register</h2>
               <form className="mb-3">
@@ -45,9 +44,9 @@ export const Register = () => {
               </form>
               <hr />
               <div className="d-flex justify-content-center">
-                <a href="/login" className="text-decoration-none">
+                <Link to="/login" className="text-decoration-none">
                   Login
-                </a>
+                </Link>
               </div>
             </div>
           </div>
